ea71aaf93c4a7d422d4f240c6e38145d5f75d60f
Saeed1112 <nullee.sa@gmail.com>
2023-09-03 19:56:13 +0330

    4

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,7 @@
 import NextAuth from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      async authorize(credentials: any) {
-        const authResponse = await fetch("/users/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(credentials),
-        });
-
-        if (!authResponse.ok) {
-          return null;
-        }
-
-        const user = await authResponse.json();
-
-        return user;
-      },
-    }),
-  ],
+  providers: [],
 });
 
 export { handler as GET, handler as POST };
