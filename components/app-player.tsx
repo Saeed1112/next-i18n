82de9fb62956141e6fe6734efd796397c43616bf
Saeed1112 <nullee.sa@gmail.com>
2023-09-10 21:27:30 +0330

    5

--- components/app-player.tsx
+++ components/app-player.tsx
@@ -13,6 +13,72 @@ const variants = {
   },
 } as any;
 
+function PlayerControls() {
+  return (
+    <div className='flex flex-1 flex-shrink-0  items-center justify-end gap-5  sm:justify-center'>
+      <Button
+        variant={'flat'}
+        isIconOnly
+        radius='full'
+        size={'sm'}
+        className='hidden sm:flex'
+      >
+        <SkipBack className='fill-gray-50' size={14} />
+      </Button>
+      <Button
+        isIconOnly
+        variant={'flat'}
+        radius='full'
+        className='justify-self-end'
+      >
+        <Play className='fill-gray-50' size={18} />
+      </Button>
+      <Button
+        isIconOnly
+        radius='full'
+        variant={'flat'}
+        size={'sm'}
+        className='hidden sm:flex'
+      >
+        <SkipForward className='fill-gray-50' size={14} />
+      </Button>
+    </div>
+  );
+}
+
+function PlayerSeeker() {
+  return (
+    <div className='h-1 rounded-t-md bg-primary/30'>
+      <div className='relative flex h-full w-1/3 rounded-t-md bg-primary'>
+        <div className='ease-[cubic-bezier(1,-0.4,0,1.4)] absolute -end-2 -top-1.5 h-4 scale-75 select-none rounded-md bg-primary px-2 text-xs font-medium transition-all duration-200 group-hover:scale-100'>
+          00:32
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PlayerTrack() {
+  return (
+    <div className='flex max-w-xs flex-shrink items-center gap-2'>
+      <Avatar
+        radius='sm'
+        size='lg'
+        className='flex-shrink-0'
+        src={'/images/the-weeknd.webp'}
+      />
+      <div className='flex flex-col justify-center overflow-hidden'>
+        <h4 className='truncate text-base font-medium'>
+          You (Acoustic) as fdasda sdas fasf asdfas f
+        </h4>
+        <span className='truncate text-xs text-gray-400'>
+          Regard, Troye Sivan & Tate McRae
+        </span>
+      </div>
+    </div>
+  );
+}
+
 export function AppPlayer() {
   const { playlistState } = useApp() as any;
 
@@ -23,34 +89,12 @@ export function AppPlayer() {
         initial={['open']}
         variants={variants}
         transition={{ type: 'tween' }}
-        className='group fixed inset-2 top-[unset] z-50 flex items-center justify-center rounded-b-md bg-black/80 fill-black/80 p-2 backdrop-blur lg:right-[--right]'
+        className='group fixed inset-2 top-[unset] z-50 flex flex-col justify-center rounded-md bg-black/80 fill-black/80 backdrop-blur lg:right-[--right]'
       >
-        <div className='absolute -top-1 left-0 right-0 h-1 rounded-t-full bg-primary/30'>
-          <div className='relative flex h-full w-1/3 rounded-s-md bg-primary'>
-            <div className='ease-[cubic-bezier(1,-0.4,0,1.4)] absolute -end-2 -top-1.5 h-4 scale-75 select-none rounded-md bg-primary px-2 text-xs font-medium transition-all duration-200 group-hover:scale-100'>
-              00:32
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-1 gap-2'>
-          <div className='flex max-w-xs items-center gap-2'>
-            <Avatar radius='sm' size='lg' src={'/images/the-weeknd.webp'} />
-            <div className='flex flex-col justify-center'>
-              <h4 className='text-base font-medium'>Out of time</h4>
-              <span className='text-xs text-gray-400'>The Weeknd</span>
-            </div>
-          </div>
-          <div className='flex flex-1 items-center justify-center gap-5'>
-            <Button variant={'flat'} isIconOnly radius='full' size={'sm'}>
-              <SkipBack className='fill-gray-50' size={16} />
-            </Button>
-            <Button isIconOnly variant={'flat'} radius='full'>
-              <Play className='fill-gray-50' size={18} />
-            </Button>
-            <Button isIconOnly radius='full' variant={'flat'} size={'sm'}>
-              <SkipForward className='fill-gray-50' size={16} />
-            </Button>
-          </div>
+        <PlayerSeeker />
+        <div className='flex flex-1 gap-2 p-2'>
+          <PlayerTrack />
+          <PlayerControls />
         </div>
       </motion.div>
     </AnimatePresence>
