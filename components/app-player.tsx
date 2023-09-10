cfd8c0972f934283ed6a5ecadcd93ed84c0c0702
Saeed1112 <nullee.sa@gmail.com>
2023-09-10 20:59:25 +0330

    8

--- components/app-player.tsx
+++ components/app-player.tsx
@@ -22,10 +22,14 @@ export function AppPlayer() {
         initial={['open']}
         variants={variants}
         transition={{ type: 'tween' }}
-        className='fixed inset-2 top-[unset] z-50 flex items-center justify-center rounded-b-md bg-black/80 fill-black/80 p-2 backdrop-blur lg:right-[--right]'
+        className='group fixed inset-2 top-[unset] z-50 flex items-center justify-center rounded-b-md bg-black/80 fill-black/80 p-2 backdrop-blur lg:right-[--right]'
       >
-        <div className='absolute -top-1 left-0 right-0 h-1 overflow-hidden rounded-t-full bg-neutral-600'>
-          <div className='relative h-full w-1/3 bg-primary'></div>
+        <div className='absolute -top-1 left-0 right-0 h-1 rounded-t-full bg-neutral-600'>
+          <div className='relative flex h-full w-1/3 rounded-s-full bg-primary'>
+            <div className='ease-[cubic-bezier(1,-0.4,0,1.4)] absolute -end-2 -top-1.5 h-4 scale-75 select-none rounded-md bg-primary px-2 text-xs font-medium transition-all duration-200 group-hover:scale-100'>
+              00:32
+            </div>
+          </div>
         </div>
         <div className='flex flex-1 gap-2'>
           <Avatar radius='sm' size='lg' src={'/images/the-weeknd.webp'} />
