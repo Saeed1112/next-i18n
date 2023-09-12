971e47b035ee0292324b145c216ba89ad6fa294c
Saeed1112 <nullee.sa@gmail.com>
2023-09-12 13:53:28 +0330

    5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,31 @@
 /** @type {import("tailwindcss").Config} */
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react');
 
 module.exports = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
-        "mona-sans-kalameh": ["var(--font-mona-sans)", "var(--font-kalameh)"]
+        'mona-sans-kalameh': ['var(--font-mona-sans)', 'var(--font-kalameh)'],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      }
-    }
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate"), nextui()]
+  plugins: [require('tailwindcss-animate'), nextui()],
 };
